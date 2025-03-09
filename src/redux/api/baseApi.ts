b151bf1c0888f55b8f004f22b2c106b3e7b26e11
sowmik-sec <sowmik.sec@gmail.com>
2025-03-09 17:42:33 +0600

    setup redux store and rehydrate with redux persist

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,4 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+// https://book-management-dashboard-server-kohl.vercel.app/api/v1
+
 import {
   BaseQueryApi,
   BaseQueryFn,
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,38 @@
 import { configureStore } from "@reduxjs/toolkit";
-// ...
+import { baseApi } from "./api/baseApi";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  persistStore,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import authReducer from "./features/auth/authSlice";
 
+const persistConfig = {
+  key: "auth",
+  storage,
+};
+
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [baseApi.reducerPath]: baseApi.reducer,
+    auth: persistedAuthReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(baseApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export const persistor = persistStore(store);
